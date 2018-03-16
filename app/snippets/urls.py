399fd59a4e4f1dd202439b39f3913d0e3c329c9f
park chan <parkchan428@gmail.com>
2018-03-16 15:23:40 +0900

    SnippetList, Detail에 format적용

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from snippets import views
+from . import apis
 
+app_name = 'snippets'
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(>P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    path('', apis.SnippetList.as_view()),
+    path('<int:pk>/', apis.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
