68be0ef3659bd3d8b7036f5017840c45a844cdda
park chan <parkchan428@gmail.com>
2018-03-19 14:13:33 +0900

    SnippetList, Detail에 permission추가

--- app/snippets/apis/generic.py
+++ app/snippets/apis/generic.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from ..models import Snippet
+from ..permissions import IsOwnerOrReadOnly
 from ..serializers import SnippetSerializer
 
 __all__ = (
@@ -12,8 +13,18 @@ __all__ = (
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+    )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
